5c0690beb56b9ee5c47eddc4bf160caeb0bac328
kirill71 <kkaysidi31@bk.ru>
2025-08-16 16:24:24 +0200

    fix main.py

--- main.py
+++ main.py
@@ -51,5 +51,7 @@ async def main():
         close_loop=False,  # чтобы не пытался закрыть активный event loop
     )
 
-if __name__ == "__main__":
-    asyncio.run(main())
+if name == "__main__":
+    import asyncio
+    asyncio.get_event_loop().create_task(main())
+    asyncio.get_event_loop().run_forever()
