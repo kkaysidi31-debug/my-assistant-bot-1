4c82bd76d75a161553222b9212cbbf66116d2d71
kirill71 <kkaysidi31@bk.ru>
2025-08-27 08:49:10 +0200

    Update main.py

--- main.py
+++ main.py
@@ -146,5 +146,5 @@ def webhook():
 # ======================
 # Запуск
 # ======================
-if name == "__main__":
+if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
